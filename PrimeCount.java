42b0f1f7647858a20ca7f97e9a6f112fc66fd733
jonny-grace <johnlemma9@gmail.com>
2025-04-07 15:46:16 +0300

    Prime number counter from start to end

--- PrimeCount.java
+++ PrimeCount.java
@@ -8,12 +8,33 @@ package MahrashiQuestion;
 public class PrimeCount {
 
     public static void main(String[] args) {
-        
+        System.out.println(primeCount(10,100));
     }
 
-    int primeCount(int start, int end){
+   static int primeCount(int start, int end){
+    //check if start is lessthan or equal to end
+   
+    if(start > end) return 0;
+    if(end < 2) return 0;
+    //check if start is -ve number then we should start from 0 
+    if(start <= 1){
+        start=2;
+    }
+    int counter=0;
+    for(int num = start; num<=end; num++){
+        boolean isPrime=true;// 2 and 10 
+         for(int i=2; i<= Math.sqrt(num); i++){
+           if(num%i==0){
+               isPrime=false;
+            break;
+           }
+         }
+         if(isPrime){
+            counter+=1;
+            isPrime=true;
+         }
+    }
     
-
-        return 0;
+    return counter;
     }
 }
