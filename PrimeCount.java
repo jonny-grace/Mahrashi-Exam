27614e23c42b9f3961ea4c1777113111efe824e0
jonny-grace <johnlemma9@gmail.com>
2025-04-07 15:00:37 +0300

    Q1. Prime Count

---
+++ PrimeCount.java
@@ -0,0 +1,19 @@
+// 1. Write a function named primeCount with signature
+// int primeCount(int start, int end);
+// The function returns the number of primes between start and end inclusive. Recall that a prime is
+// a positive integer greater than 1 whose only integer factors are 1 and itself.
+
+package MahrashiQuestion;
+
+public class PrimeCount {
+
+    public static void main(String[] args) {
+        
+    }
+
+    int primeCount(int start, int end){
+    
+
+        return 0;
+    }
+}
